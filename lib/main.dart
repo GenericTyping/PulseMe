3aa9a40cdafd520fa69954026f0c54f4ad64c5e0
Jeroen Meijer <jeroenfkmeijer@gmail.com>
2018-07-08 02:58:12 +0200

    Added basic Firebase Cloud Messaging integration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:pulseme/views/pages/home_page.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-void main() => runApp(new PulseMeApp());
+void main() {
+  runApp(new PulseMeApp());
+}
 
 class PulseMeApp extends StatelessWidget {
+  PulseMeApp() {
+    final FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
+    _firebaseMessaging.requestNotificationPermissions();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) {
+        debugPrint("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) {
+        debugPrint("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) {
+        debugPrint("onResume: $message");
+      },
+    );
+
+    _firebaseMessaging.getToken().then((token) => debugPrint("Token: $token"));
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
